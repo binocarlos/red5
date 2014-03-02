13852caf9cd64b0de07cf45c12881779c66ecb33
Kai Davenport <kaiyadavenport@gmail.com>
2014-03-02 14:18:17 +0000

    install node

--- bin/installnode.sh
+++ bin/installnode.sh
@@ -3,9 +3,10 @@
 VERSION=0.11.0
 PLATFORM=linux
 ARCH=arm-pi
-PREFIX="/usr/local/node-v$VERSION-$PLATFORM-$ARCH"
- 
-mkdir -p "$PREFIX" && \
-curl http://nodejs.org/dist/v$VERSION/node-v$VERSION-$PLATFORM-$ARCH.tar.gz \
-  | tar xzvf - --strip-components=1 -C "$PREFIX"
-export PATH=$PATH:$HOME/node-v$VERSION-$PLATFORM-$ARCH/bin
+NODE="node-v$VERSION-$PLATFORM-$ARCH"
+
+mkdir -p "/tmp/nodeversions/$VERSION"
+
+curl http://nodejs.org/dist/v$VERSION/$NODE.tar.gz \
+  | tar xzvf - --strip-components=1 -C "/tmp/nodeversions/$VERSION"
+#export PATH=$PATH:$PREFIX/node-v$VERSION-$PLATFORM-$ARCH/bin
